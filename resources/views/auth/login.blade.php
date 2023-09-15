a5939b9190e21927b2fd74faf988c5eb4c012436
reza <rdaqrbanzadh206@gmail.com>
2023-09-15 10:27:53 -0700

    edit login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,116 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<head>
+    <title>Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="{{asset("loginregister/assets/images/icons/favicon.ico")}}" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset("loginregister/assets/vendor/bootstrap/css/bootstrap.min.css")}}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset("loginregister/assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css")}}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset("loginregister/assets/vendor/animate/animate.css")}}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset("loginregister/assets/vendor/css-hamburgers/hamburgers.min.css")}}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset("loginregister/assets/vendor/select2/select2.min.css")}}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset("loginregister/assets/css/util.css")}}">
+    <link rel="stylesheet" type="text/css" href="{{asset("loginregister/assets/css/main.css")}}">
+    <!--===============================================================================================-->
+</head>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+<body>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<div class="limiter">
+    <div class="container-login100">
+        <div class="wrap-login100">
+            <div class="login100-pic js-tilt" data-tilt>
+                <img src="{{asset("loginregister/assets/images/img-01.png")}}" alt="IMG">
             </div>
+
+            <form method="post" action="{{ route('login') }}" class="login100-form validate-form">
+                @csrf
+                    <span class="login100-form-title">
+                        Member Login
+                    </span>
+
+                <div class="mb-2 alert alert-danger"> <small class="form-text text-danger"> خطا</small> </div>
+
+
+                <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+                    <input class="input100 @error('email') is-invalid @enderror" value="{{ old('email') }}" type="text" name="email" placeholder="Email">
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+                            <i class="fa fa-envelope" aria-hidden="true"></i>
+                        </span>
+                </div>
+
+                <div class="wrap-input100 validate-input" data-validate="Password is required">
+                    <input class="input100  @error('password') is-invalid @enderror" type="password" name="password" placeholder="Password">
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+                            <i class="fa fa-lock" aria-hidden="true"></i>
+                        </span>
+                </div>
+
+                <div class="container-login100-form-btn">
+                    <button type="submit" class="login100-form-btn">
+                        Login
+                    </button>
+                </div>
+
+                <div class="text-center p-t-12">
+                        <span class="txt1">
+                            Forgot
+                        </span>
+                      <a class="btn btn-link" href="{{ route('password.request') }}">
+                          {{ __(' Your Password?') }}
+                       </a>
+                </div>
+
+                <div class="text-center p-t-136">
+                    <a class="txt2" href="#">
+                        Create your Account
+                        <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </form>
         </div>
     </div>
 </div>
-@endsection
+
+
+
+<script src="{{asset("loginregister/assets/vendor/jquery/jquery-3.2.1.min.js")}}"></script>
+<script src="{{asset("loginregister/assets/vendor/bootstrap/js/popper.js")}}"></script>
+<script src="{{asset("loginregister/assets/vendor/bootstrap/js/bootstrap.min.js")}}"></script>
+<script src="{{asset("loginregister/assets/vendor/select2/select2.min.js")}}"></script>
+<script src="{{asset("loginregister/assets/vendor/tilt/tilt.jquery.min.js")}}"></script>
+<script src="{{asset("loginregister/assets/js/main.js")}}"></script>
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
