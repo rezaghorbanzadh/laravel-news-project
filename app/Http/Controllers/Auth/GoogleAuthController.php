3d10441ba055f4eea367153bc830051af52ed1d3
reza <rdaqrbanzadh206@gmail.com>
2023-09-26 02:40:54 -0700

    edit function callback google register

--- app/Http/Controllers/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
+
+use function Laravel\Prompts\password;
 
 class GoogleAuthController extends Controller
 {
@@ -14,6 +18,24 @@ class GoogleAuthController extends Controller
 
     }
     public function callback(){
+        try {
+            $googleUser=Socialite::driver('google')->user();
+            $user=User::where('email',$googleUser->email)->first();
+
+            if ($user){
+                auth()->loginUsingId($user->id);
+            }else{
+                $newUser=User::create([
+                    'name'=>$googleUser->name,
+                    'email'=>$googleUser->email,
+                    'password'=>bcrypt(Str::random(16))
+                ]);
+                auth()->loginUsingId($newUser->id);
+            }
+            return redirect("/");
+        }catch (\Exception $e){
+            return $e;
+        }
 
     }
 }
